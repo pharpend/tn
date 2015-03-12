f0b1dad228b314e836c7257199de92fdf5a7464c
Peter Harpending <peter@harpending.org>
2015-03-12 15:17:21 -0600

    Poorly attempt to make the program do something
    currently it pops open the editor, but I can't figure out how to get the result.
    
    Thinking I'm going to have to use Pipes. Reading tutorial now.

--- src/Main.hs
+++ src/Main.hs
@@ -31,11 +31,68 @@ today = utctDay <$> getCurrentTime
 todayMinus :: Integer -> IO Day
 todayMinus i = addDays (-1 * i) <$> today
 
+editEntry :: Day -> IO ()
+editEntry dy = do
+  let daystr = show dy
+  withSystemTempFile thisApp launchEditor
+  putStr "Your editor is: \n    "
+  putStrLn =<< editor
+
+launchEditor :: FilePath -> Handle -> IO ()
+launchEditor fp h = do
+  hPutStrLn h "HAHAHA"
+  hPutStrLn stdout =<< hGetContents h
+  -- myEditor <- editor
+  -- ph <- runCommand $ myEditor <> " " <> fp
+  -- waitForProcess ph >>= \case
+  --   ExitSuccess -> 
+      
+  --   q -> hPutStrLn stderr "Editor failed" >> exitWith q
+
+editToday :: IO ()
+editToday = editEntry =<< today
+
+help = putStrLn "yay"
+
+tnVersion :: IO ()
+tnVersion = print version
+
+editor :: IO String
+editor = tryIOError (getEnv "EDITOR") >>= \case
+           Left _  -> return "nano"
+           Right e -> return e
+
+tnDir :: IO FilePath
+tnDir = getAppUserDataDirectory thisApp
+
+getHypotheticalDataFileName :: String -> IO FilePath
+getHypotheticalDataFileName s = do
+  dir <- tnDir
+  return $ dir <> s
+
 main :: IO ()
-main = print =<< myJournal
-  where
-    myJournal :: IO Journal
-    myJournal = do
-      tdy <- today
-      ytdy <- todayMinus 1
-      return $ Map.fromList [(tdy, "Yay trivial entry"), (ytdy, "Yay another trivial entry")]
+main = do
+  args <- getArgs
+  if or ["--help" `elem` args, "-h" `elem` args]
+    then help
+    else if "--version" `elem` args
+      then tnVersion
+      else runTn
+
+runTn :: IO ()
+runTn = do
+  args <- getArgs
+  let fstarg = headMay args
+      rstof = tailMay args
+  case fstarg of
+    Nothing -> editToday
+    Just cmd ->
+      case cmd of
+        "edit" ->
+          case rstof of
+            Just (s:_) ->
+              case readMay s of
+                Nothing -> help
+                Just d  -> editEntry d
+            _ -> help
+        _ -> help

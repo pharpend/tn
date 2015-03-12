6ca4653a6106bf7ba46e26f38945b5d39e201bdc
Peter Harpending <peter@harpending.org>
2015-03-12 13:12:51 -0600

    tn now prints out a trivial journal.
    This is mostly just to experiment.

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,28 @@
 module Main where
 
+import           Control.Applicative
+import           Data.Data
+import qualified Data.Map.Lazy as Map
 import qualified Data.Text as T
 import           Data.Time
+import           Data.Typeable
+import           System.Environment
+import           System.IO
 
-data Journal = Journal { date :: UTCTime
-                       , text :: T.Text
-                       }
+type Journal = Map.Map Day Entry
+type Entry = T.Text
+
+today :: IO Day
+today = utctDay <$> getCurrentTime
+
+todayMinus :: Integer -> IO Day
+todayMinus i = addDays (-1 * i) <$> today
 
 main :: IO ()
-main = return ()
+main = print =<< myJournal
+  where
+    myJournal :: IO Journal
+    myJournal = do
+      tdy <- today
+      ytdy <- todayMinus 1
+      return $ Map.fromList [(tdy, "Yay trivial entry"), (ytdy, "Yay another trivial entry")]

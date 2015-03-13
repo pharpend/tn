e2db05ebc136e7d9ce4b3c5994dc2e1f570328d3
Peter Harpending <peter@harpending.org>
2015-03-13 10:58:08 -0600

    Remove unnecessary imports in Tn.Static

--- src/Tn/Static.hs
+++ src/Tn/Static.hs
@@ -30,26 +30,14 @@
 
 module Tn.Static where
 
-import           Control.Applicative
-import qualified Data.ByteString as B
-import           Data.Aeson
-import           Data.Aeson.Encode.Pretty
-import           Data.Char
-import           Data.Data
 import qualified Data.Map.Lazy as Map
-import           Data.Monoid
 import qualified Data.Text as T
 import           Data.Time
-import           Data.Typeable
 import           Data.Version
 import           Paths_tn (version)
-import           Safe
 import           System.Directory
 import           System.Environment
-import           System.Exit
-import           System.IO
 import           System.IO.Error
-import           System.Process
 
 -- |== Less boring part
 -- 
@@ -100,4 +88,3 @@ help = putStrLn "No help for you"
 -- | Will output @tn v.4.12.4@, if @4.12.4@ is the version.
 tnVersion :: String
 tnVersion = showVersion version
-

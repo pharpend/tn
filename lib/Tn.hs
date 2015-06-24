b710099f81d3557bf90ce52cee9197c3a0768324
Peter Harpending <peter@harpending.org>
2015-06-24 12:14:33 -0600

    Redo dir structure for rewrite.

--- lib/Tn.hs
+++ lib/Tn.hs
@@ -1,14 +1,12 @@
 -- | 
--- Module      : Skel
--- Description : Short description
+-- Module      : Tn
+-- Description : Main module for the tn library
 -- Copyright   : Copyright (c) 2014-2015, Peter Harpending.
 -- License     : BSD-2
 -- Maintainer  : Peter Harpending <peter@harpending.org>
 -- Stability   : experimental
 -- Portability : POSIX
 -- 
--- Here is a longer description of this module, containing some
--- commentary with @some markup@.
 
-module Skel where
+module Tn where
 
--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,13 @@
 -- | 
--- Module      : Skel
--- Description : Short description
+-- Module      : Main
+-- Description : Main module for tn
 -- Copyright   : Copyright (c) 2014-2015, Peter Harpending.
 -- License     : BSD-2
 -- Maintainer  : Peter Harpending <peter@harpending.org>
 -- Stability   : experimental
 -- Portability : POSIX
--- 
--- Here is a longer description of this module, containing some
--- commentary with @some markup@.
 
-module Skel where
+module Main where
 
+main :: IO ()
+main = return ()

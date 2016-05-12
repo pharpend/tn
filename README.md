9a3c34bf7a9cd90947a81bb130a1882085da3140
Peter Harpending <peter@harpending.org>
2016-05-11 20:55:51 -0600

    Introducing new, spaghetti-esque script!
    It's awful and needs to be refactored. But it works!

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 # tn
 
-This is a command-line program for keeping your own journals. It's
-licensed under the [GPL version 3][gpl-gnu], which you can find in the
-[LICENSE file][license-file] Tn is written in Haskell. It's not yet
-usable, so don't try to use it.
+This is a command-line program for keeping your own journals, written in
+[Haskell][hs] and licensed under the [ISC license][iscl]. You can find a
+copy of the ISC licensein the [LICENSE file][license-file]
 
 ## History
 
@@ -21,26 +20,14 @@ using the Colemak layout. Hitting the key immediately under left index
 finger, followed by the key under the right index finder yielded the
 sequence `tn` in Colemak.
 
-## Plans for version 4
+## Specification
 
-So, here are my plans for version 4, which I may or may not follow
+*   `tn "some string"` records the journal entry to
+    `~/.local/share/tn/journal.yml` with the time.
 
-* The program, at its core, allows you to keep journal entries, written
-  in [Markdown][mkdn], and optionally encrypted.
-* Tn will, via [Pandoc][pandoc], allow the user a variety of export
-  formats. So, if you want to print your journal, you could output it as
-  an HTML file or a word processor document. If you wanted a more
-  external-program-friendly format, tn will simply allow you to export
-  your journal as a tarball.
-* Tn should, by default, store the data in a XDG-correct location, such
-  as `~/.local/share/tn` on UNIX-like systems, or whatever the
-  equivalent is on Windows.
-* Tn will eventually allow multiple journals, as well as a hierarchy of
-  journals.
-* Tn will allow that practice of replacing `tn some-long-command` with
-  `tn slc`.
-* Tn should at some point allow a graphical interface, so that people
-  besides UNIX nerds can use it.
+    * adding `--stdout` prints the output to stdout
+    * adding `-` takes the input from stdin
+    * adding `--stdin` takes the input from stdin
   
 ## Installing
 
@@ -65,27 +52,12 @@ There is no usage yet! Check back later!
 
 You can contact me at `peter@harpending.org`, or `pharpend` on FreeNode.
 
-## Copyright
-
-Copyright (C) 2014-2016 Peter Harpending
-
-This program is free software: you can redistribute it and/or modify it
-under the terms of the GNU General Public License as published by the
-Free Software Foundation, either version 3 of the License, or (at your
-option) any later version.
-
-This program is distributed in the hope that it will be useful, but
-WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
-Public License for more details.
-
-You should have received a copy of the GNU General Public License along
-with this program. If not, see <http://www.gnu.org/licenses/>.
-
 [colemak]: http://colemak.com/
 [git]: https://git-scm.com/book/en/v2/Getting-Started-Installing-Git
 [github-bug-tracker]: https://github.com/pharpend/tn/issues
 [gpl-gnu]: https://gnu.org/licenses/
+[iscl]: https://www.haskell.org/
+[hs]: https://www.haskell.org/
 [hs-stack]: http://docs.haskellstack.org/en/stable/README.html
 [mkdn]: https://en.wikipedia.org/wiki/Markdown
 [license-file]: LICENSE
---
+++ bin/Main.hs
@@ -0,0 +1,139 @@
+module Main where
+
+import           Data.Aeson hiding (encode)
+import qualified Data.ByteString as B
+import           Data.FileEmbed
+import           Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
+import qualified Data.Text.IO as T
+import           Data.Time
+import           Data.Vector (Vector)
+import qualified Data.Vector as V
+import           Data.Yaml
+import           GHC.Generics
+import           Options.Applicative.Simple
+import qualified Paths_tn as P
+import           System.Directory
+import           System.IO
+import           System.Pager
+import           Turtle hiding (FilePath, switch, stdin, stdout)
+
+main :: IO ()
+main =
+  do (_, cmd) <-
+       simpleOptions $(simpleVersion P.version)
+                     appName
+                     "Simple journaling program"
+                     (pure ())
+                     parser
+     runCommand cmd
+  where
+    appName = "tn"
+    parser = do licenseCmd
+                newEntryCommand
+    licenseCmd =
+      addCommand "license"
+                 "Show the license (ISC)"
+                 ShowLicense
+                 (switch (mconcat [ long "no-pager"
+                                  , help "Do not pipe to the system pager"
+                                  , showDefault
+                                  ]))
+    newEntryCommand =
+      addCommand "new"
+                 "Add a new entry"
+                 id
+                 (NewEntry <$> altConcat [ fmap Argument
+                                                (strArgument (mconcat [ help "Entry text"
+                                                                      , metavar "TEXT"
+                                                                      ]))
+                                         , fmap FromFile
+                                                (strOption (mconcat [ help "Input file"
+                                                                    , metavar "PATH"
+                                                                    , long "input"
+                                                                    , short 'i'
+                                                                    ]))
+                                         , flag' Stdin
+                                                 (mconcat [ help "Read entry text from stdin"
+                                                          , long "stdin"
+                                                          ])
+                                         ]
+                           <*> altConcat [ flag' Stdout
+                                                 (mconcat [ help "Print to stdout instead of a file"
+                                                          , long "stdout"
+                                                          ])
+                                         , fmap ToFile
+                                                (strOption (mconcat [ help "Output file"
+                                                                    , metavar "PATH"
+                                                                    , long "output"
+                                                                    , short 'o'
+                                                                    ]))
+                                         , pure DefaultOutput
+                                         ])
+    altConcat = foldr (<|>) empty
+    runCommand =
+      \case
+        ShowLicense noPager
+          | noPager -> T.putStrLn licenseText
+          | otherwise -> printOrPage licenseText
+        NewEntry i o ->
+          do inputText <-
+               case i of
+                 Argument s -> return (T.pack s)
+                 FromFile fp ->
+                   do absPath <- makeAbsolute fp
+                      T.readFile absPath
+                 Stdin -> T.hGetContents stdin
+             cjp <- defaultPath
+             currentJournal <-
+               decodeFileEither cjp
+               >>= \case
+                     Left e -> fail (show e)
+                     Right x -> return x
+             currentTime <- getCurrentTime
+             let newJournal = V.snoc currentJournal
+                                     (Entry currentTime inputText)
+                 newJournalYml = encode newJournal
+             case o of
+               Stdout -> B.hPut stdout newJournalYml
+               ToFile f ->
+                 do absFile <- makeAbsolute f
+                    B.writeFile absFile newJournalYml
+               DefaultOutput ->
+                 B.writeFile cjp newJournalYml
+    licenseText = T.decodeUtf8 $(embedFile "LICENSE")
+    dataPath = getXdgDirectory XdgData appName
+    defaultPath =
+      do dp <- dataPath
+         createDirectoryIfMissing True dp
+         let pth = mappend dp "/journal.yaml"
+         fexists <- doesFileExist pth
+         if fexists
+           then return pth
+           else do T.writeFile pth "[]"
+                   return pth
+
+data Command = NewEntry Input Output
+             | ShowLicense Bool
+  deriving (Eq, Show)
+
+data Input = Argument String
+           | FromFile FilePath
+           | Stdin
+  deriving (Eq, Show)
+
+data Output = ToFile FilePath
+            | Stdout
+            | DefaultOutput
+  deriving (Eq, Show)
+
+type Journal = Vector Entry
+
+data Entry = Entry { entryTime :: UTCTime
+                   , entryText :: Text
+                   }
+  deriving (Eq, Show, Generic)
+
+instance ToJSON Entry
+instance FromJSON Entry
